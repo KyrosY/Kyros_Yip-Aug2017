7d8b43e37e786c92669d94581d638de1855857ae
KyrosY <kyrosyip@gmail.com>
2017-08-22 09:28:00 -0700

    Second Project -- using Git from Eclipse

---
+++ Project2/src/Project2.java
@@ -0,0 +1,15 @@
+
+//Kyros Yip
+//My second java project. It will compare print and printIn
+//August 22, 2017
+
+public class Project2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello -- this is project 2!!!"); 
+		
+		System.out.print("hello -- this is project 2!!!");
+		
+	}
+
+}
