c619e2e7a0292f272c33dd8a0465618fb1af31a0
KyrosY <kyrosyip@gmail.com>
2017-09-05 09:27:30 -0700

    Calculate and DoMath

---
+++ Calculate Library/src/Calculate.java
@@ -0,0 +1,30 @@
+/* Kyros Yip
+ * September 5, 2017
+ * This is our homegrown library that will contain various mathematical methods.
+*/
+public class Calculate {
+	
+/*This method takes an integer and returns its square.
+ * 
+ */
+	public static int square (int operand) {
+		return operand * operand;
+	}
+	public static int cube (int operand) {	
+		return operand * operand * operand;
+	}	
+	public static double average (double operand1, double operand2, double operand3) {
+		double average = (operand1 + operand2 + operand3)/3;
+		return average;
+	}
+	public static double toDegrees (double radian) {
+		double degree = ((radian * 100)/3.14159);
+		return degree;
+	}
+	public static double toRadians (double degree) {
+		double radian = ((3.14159*degree)/100);
+		return radian;
+	}
+}
+
+	
