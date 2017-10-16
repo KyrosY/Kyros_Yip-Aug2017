fda48493859a51431bb68022dfd7a71e5d9dc8c3
KyrosY <kyrosyip@gmail.com>
2017-10-16 15:06:10 -0700

    vhj

---
+++ QuadraticDescriber/src/Quadratic.java
@@ -0,0 +1,7 @@
+
+public class Quadratic {
+	public static String quadrDescriber(double a, double b, double c) {
+		String s = "Description of the graph of: \n" + "y = " + a + "x^2 + " + b + "x + " + c;
+		return s;
+				}
+}
---
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class QuadraticClient {
+	public static void main (String [] args) {
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("Welcome to the Quadratic Describer");
+		System.out.println("Provide values for the coefficients a, b, and c");
+		System.out.println();
+		System.out.println("a: ");
+		double a = userInput.nextDouble();
+		System.out.println("b: ");
+		double b = userInput.nextDouble();
+		System.out.println("c: ");
+		double c = userInput.nextDouble();
+		System.out.println();
+		
+	}	
+	}
