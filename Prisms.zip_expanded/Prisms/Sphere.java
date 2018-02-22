59423835362e1837fe257ca505b2194b8179a4e8
KyrosY <19yipk83@millshighsch.org>
2018-02-22 08:44:16 -0800

    Spheres

--- Prisms.zip_expanded/Prisms/Sphere.java
+++ Prisms.zip_expanded/Prisms/Sphere.java
@@ -1,4 +1,18 @@
 
-public class Sphere {
+public class Sphere extends ThreeDShape
+{
+	private double radius;
+	
+	public Sphere (double radius) {
+		this.radius = radius;
+	}
+	public double calcVolume()
+	{
+		return ((4 * Math.PI * radius * radius * radius) / 3);
+	}
 
+	public double calcSA()
+	{
+		return 4 * Math.PI *radius * radius;
+	}	
 }
